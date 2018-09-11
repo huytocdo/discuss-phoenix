8dc55cdb93ddc79852a860f30d2c8174554748c9
huytocdo <ndhuyspkt@gmail.com>
2018-09-11 22:45:19 +0700

    update sign_in and sign_out function

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "61f7ad1408f4d04544d1",
+  client_secret: "d97113fae9db2cabed379fb3d738532860ad5a1d"
